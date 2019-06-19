72f9a2e9695b51838f3010dfd9212e3cd206bd2e
Miguelangel Parra <miguelangelparra@MacBook-Pro-de-Daniel.local>
2019-06-19 17:05:41 -0300

    agregado funcion suma total y suma gasto

--- Registro de gastos/assets/js/funcionalidad.js
+++ Registro de gastos/assets/js/funcionalidad.js
@@ -10,45 +10,59 @@ const app = new Vue({
       fecha: new Date().toLocaleDateString(),
       usuario: "",
       concepto: "",
-      unidad: "",
       cantidad: 0,
       precio:  0,
       total: 0,
       categoria: "",   
       lugar: "",
-    }
+    },
+    cantidadGastos:0,
+    totalGastos:0
   },
 
   methods: {
     insertarGasto() {
+      
       this.gastos.push(
         {
           id: this.gastoNuevo.id,
           fecha: this.gastoNuevo.fecha,
           usuario: this.gastoNuevo.usuario,
           concepto: this.gastoNuevo.concepto,
-          unidad: this.gastoNuevo.unidad,
           cantidad: this.gastoNuevo.cantidad,
           precio: this.gastoNuevo.precio,
           total: this.gastoNuevo.cantidad * this.gastoNuevo.precio,
           categoria: this.gastoNuevo.categoria,
-          lugar: this.gastoNuevo.lugar
-          
+          lugar: this.gastoNuevo.lugar,      
         }
       )
       this.gastoNuevo.id = ""
       this.gastoNuevo.fecha = new Date().toLocaleDateString(),
       this.gastoNuevo.usuario= ""
       this.gastoNuevo.concepto=""
-      this.gastoNuevo.unidad=""
       this.gastoNuevo.cantidad=0
       this.gastoNuevo.precio= 0
       this.gastoNuevo.total= 0
       this.gastoNuevo.categoria=""
       this.gastoNuevo.lugar=""
+    },
+    modificarGasto(){
+      alert("Presionaste btn Modificar")
+    },
+    eliminarGasto(){
+      alert("Presionaste btn eliminar")
     }
   },
 
 
-  computed: {}
+  computed: {
+sumaTotal (){
+  this.totalGastos=0
+  for(gasto of this.gastos){this.totalGastos += gasto.total} return this.totalGastos
+},
+sumaGastos(){
+  this.cantidadGastos = this.gastos.length
+  return this.cantidadGastos
+}
+  }
 })
